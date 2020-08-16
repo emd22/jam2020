bb9d4e5728847de146a59e190d1fa8fc83a6f764
Andrew MacDonald <andrew@getreal.band>
2020-08-16 02:47:44 -0300

    Union types

--- interpreter/type.py
+++ interpreter/type.py
@@ -1,9 +1,26 @@
+
+
 class Type:
   def __init__(self, name, parent=None, members={}, nominative=False):
     self.name = name
     self.parent = parent
     self.members = members
     self.nominative = nominative
+
+    def union(self, other):
+      return UnionType(self, other)
+
+  # def merge(self, other, new_name):
+  #   union_members = self.members.copy()
+
+  #   for (name, value) in other.members:
+  #     if name in union_members:
+  #       if not value.compare_type(union_members[name]):
+  #         union_members[name] = UnionType(union_members[name], value)
+  #     else:
+  #       union_members[name] = value
+
+  #   return Type(new_name, )
 
   def compare_type(self, other_type):
     if other_type == self:
@@ -45,7 +62,28 @@ class Type:
       return None
 
     return self.members[name]
-
-
-
     
+class UnionType(Type):
+  def __init__(self, name, lhs, rhs):
+    self.name = name
+    self.lhs = lhs
+    self.rhs = rhs
+
+  def compare_type(self, other_type):
+    # TODO test this, might have to compare lhs,rhs as one against other_type.
+    return self.lhs.compare_type(other_type) or self.rhs.compare_type(other_type)
+
+  def has_property(self, name, property_type=None):
+    return self.lhs.has_property(name, property_type) or self.rhs.has_property(name, property_type)
+
+  def get_property_type(self, name):
+    if self.lhs.has_property(name):
+      if (self.rhs.has_property(name)):
+        return UnionType(self.lhs.get_property_type(name), self.rhs.get_property_type(name))
+      else:
+        return self.lhs.get_property_type(name)
+
+    if self.rhs.has_property(name):
+      return self.rhs.get_property_type(name)
+
+    return None
